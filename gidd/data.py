94c120938f93a9aabd0bb15230d8ef3f41657dc7
prisold <pascal.risold@gmail.com>
2025-05-06 15:29:06 +0200

    fixed sudoku train test splitting

--- gidd/data.py
+++ gidd/data.py
@@ -19,9 +19,10 @@ def get_dataset(config, num_proc=32):
     n_proc = min(os.cpu_count(), num_proc)
     if config.data.local_dataset:
         # TODO?: run dataloading script to download and prepare the dataset if it does not exist
-        ds = load_from_disk(f"datasets/{config.data.dataset_name}{('_' + config.data.dataset_subset) if config.data.dataset_subset else ''}/train")
-        train_ds = ds[:-test_size]
-        test_ds = ds[-test_size:]
+        ds = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/{config.data.dataset_name}{('_' + config.data.dataset_subset) if config.data.dataset_subset else ''}/train")
+        ds = ds.train_test_split(test_size=test_size)
+        train_ds = ds['train']
+        test_ds = ds['test']
     else:
         train_ds = load_dataset(
             config.data.dataset_name,
