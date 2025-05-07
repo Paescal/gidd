0814de5a069bd2dd45ee1eb8f679de3d5864ac14
prisold <pascal.risold@gmail.com>
2025-05-07 16:28:08 +0200

    support for padded and non-padded tokenizer vocab size

--- gidd/models/dit.py
+++ gidd/models/dit.py
@@ -358,7 +358,13 @@ class DIT(nn.Module, huggingface_hub.PyTorchModelHubMixin):
 
     self.config = config
     self.vocab_size = vocab_size
-    self.rounded_vocab_size = vocab_size + (128 - vocab_size % 128) % 128
+    # TODO: Whether or not to pad the vocab size is not clear. Test both cases.
+    # If padded, the tokenizer needs to be padded as well.
+    round_vocab = True
+    if round_vocab:
+      self.rounded_vocab_size = vocab_size
+    else:
+      self.rounded_vocab_size = vocab_size + (128 - vocab_size % 128) % 128
 
     self.vocab_embed = EmbeddingLayer(config.model.hidden_size, self.rounded_vocab_size)
     self.sigma_map = TimestepEmbedder(config.model.cond_dim)
