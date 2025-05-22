0d17b1b679ed5c3baba28677067e62e3c3d7f5d2
prisold <pascal.risold@gmail.com>
2025-05-22 23:57:22 +0200

    ready to train with settings like MDM+schedule

--- gidd/data.py
+++ gidd/data.py
@@ -20,10 +20,14 @@ def get_dataset(config, num_proc=32):
     if config.data.local_dataset:
         # TODO?: run dataloading script to download and prepare the dataset if it does not exist
         # expects sudoku.yaml to contain e.g. dataset_name: sudoku, dataset_subset: 3m
-        ds = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/{config.data.dataset_name}{('_' + config.data.dataset_subset) if config.data.dataset_subset else ''}/train")
-        ds = ds.train_test_split(test_size=test_size)
-        train_ds = ds['train']
-        test_ds = ds['test']
+        # ds = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/{config.data.dataset_name}{('_' + config.data.dataset_subset) if config.data.dataset_subset else ''}/train")
+        # ds = ds.train_test_split(test_size=test_size)
+        # train_ds = ds['train']
+        # test_ds = ds['test']
+        # score_ds = test_ds
+
+        train_ds = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/sudoku_shah/easy/train")
+        test_ds = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/sudoku_shah/easy/test")
         score_ds = test_ds
         
         # expects sudoku.yaml to contain e.g. dataset_name: gidd/datasets/sudoku/train/, dataset_subset: 3m
