bff85945bd799d21be2efc9fedf6ccc474ea3ba3
prisold <pascal.risold@gmail.com>
2025-05-25 13:05:25 +0200

    easily build tokenizers with different vocab sizes

--- gidd/tokenizers/build_tokenizers.py
+++ gidd/tokenizers/build_tokenizers.py
@@ -3,17 +3,19 @@ from tokenizers.models import WordLevel
 from tokenizers.pre_tokenizers import Split
 from transformers import PreTrainedTokenizerFast
 
-def build_sudoku_tokenizer():
-    # path = "gidd/tokenizers/sudoku"
-    path = "gidd/tokenizers/sudoku_padded"
+def build_sudoku_tokenizer(pad_vocab_to=0):
+    if pad_vocab_to > 0:
+        path = "gidd/tokenizers/sudoku_padded"
+    else:
+        path = "gidd/tokenizers/sudoku"
+    
     tokenizer_json = "/tokenizer.json"
     
     digits = "123456789"
-    # vocab = list(digits) + ["[PAD]", "0", "[UNK]", "[BOS]", "[EOS]"]
     core_vocab = list(digits)
-    special_tokens = ["[PAD]", "0", "[UNK]", "[BOS]", "[EOS]"]
-    padded_vocab_len = 128
-    tokens_to_pad_vocab_length = [f"[VPAD{i}]" for i in range(padded_vocab_len - len(core_vocab) - len(special_tokens))]
+    # special_tokens = ["0", "[PAD]", "[UNK]", "[BOS]", "[EOS]"]
+    special_tokens = ["[PAD]","0", "[UNK]", "[BOS]", "[EOS]"]
+    tokens_to_pad_vocab_length = [f"[VPAD{i}]" for i in range(pad_vocab_to - len(core_vocab) - len(special_tokens))]
     vocab = core_vocab + special_tokens + tokens_to_pad_vocab_length
 
     tokenizer = Tokenizer(WordLevel({k: i for i, k in enumerate(vocab)}, unk_token="[UNK]"))
@@ -24,4 +26,5 @@ def build_sudoku_tokenizer():
     fast_tokenizer.add_special_tokens({"pad_token": "[PAD]", "mask_token": "0", "unk_token": "[UNK]", "bos_token": "[BOS]", "eos_token": "[EOS]"})
     fast_tokenizer.save_pretrained(path)
 
-build_sudoku_tokenizer()
+# build_sudoku_tokenizer()
+# build_sudoku_tokenizer(pad_vocab_to=128)
