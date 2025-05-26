fixed numerical precision issues when calling categorical sampling for padded vocab

gidd/diffusion_process.py
@@ -149,7 +149,7 @@ class HybridDiffusion(NoiseSchedule):
     def sample_zt(self, input_ids, diffusion_mask, t):
         x = F.one_hot(input_ids, num_classes=self.vocab_size_architecturally).to(dtype=t.dtype)
         probs = self.probs_at_t(x, t)
-        z_t = sample_categorical(probs)
+        z_t = sample_categorical(probs, end_index=self.vocab_size_semantically - 1)
         z_t = torch.where(diffusion_mask.to(torch.bool), z_t, input_ids)
         return z_t
     

gidd/utils.py
@@ -144,11 +144,11 @@ def sample_position_top_k_gumbel(metric, k, gumbel_noise_coefficient=0, generato
     return top_k_mask
 
 @torch.no_grad()
-def sample_categorical(probs, generator=None):
+def sample_categorical(probs, end_index=-1, generator=None):
     # return torch.distributions.Categorical(probs=probs).sample()
     uniform = torch.rand(probs.shape[:-1], dtype=probs.dtype, device=probs.device, generator=generator).unsqueeze(-1)
     cumprobs = probs.cumsum(-1)
-    cumprobs[..., -1] = 1 + 1e-4
+    cumprobs[..., end_index:] = 1 + 1e-4
     samples = torch.searchsorted(cumprobs, uniform, right=True).squeeze(-1)
     return samples
 