f4428db00551a5a8a3d6ff977747cb03c9918cb0
prisold <pascal.risold@gmail.com>
2025-05-30 18:27:02 +0200

    added comparison between different models

--- gidd/eval/evaluate_sampling_strategies.py
+++ gidd/eval/evaluate_sampling_strategies.py
@@ -38,12 +38,26 @@ def main(config):
     torch.set_float32_matmul_precision('high')
     torch.set_grad_enabled(False)
 
-    ckpt_path = hydra.utils.to_absolute_path(config.checkpoint_path)
+    ckpt_paths = config.checkpoint_path
+    models = []
+    noise_schedules = []
+    tokenizer = None
+    ckpt_configs = []
+    dtype = None
 
-    model, noise_schedule, tokenizer, ckpt_config = load_checkpoint(ckpt_path, device=device)
-    model.eval()
-    ckpt_config.training.eval_batch_size = config.batch_size
-    dtype = parse_dtype(ckpt_config.training.dtype)
+    for ckpt_path in ckpt_paths:
+        ckpt_path = hydra.utils.to_absolute_path(ckpt_path)
+
+        model, noise_schedule, ckpt_tokenizer, ckpt_config = load_checkpoint(ckpt_path, device=device)
+        model.eval()
+        ckpt_config.training.eval_batch_size = config.batch_size
+        if tokenizer is None:
+            tokenizer = ckpt_tokenizer
+        if dtype is None:
+            dtype = parse_dtype(ckpt_config.training.dtype)
+        models.append(model)
+        noise_schedules.append(noise_schedule)
+        ckpt_configs.append(ckpt_config)
 
     # ds_eval = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/{ckpt_config.data.dataset_name}{('_' + ckpt_config.data.dataset_subset) if ckpt_config.data.dataset_subset else ''}/evaluate")
     ds_eval_easy = load_from_disk(f"/local/home/prisold/gidd/gidd/datasets/sudoku_shah/easy/test")
@@ -77,60 +91,62 @@ def main(config):
     top_k_gumbel_noise_coefficient = config.sampling.position_sampling_strategy_args.top_k_gumbel.gumbel_noise_coefficient
     token_sampling_strategies = config.sampling.token_sampling_strategy
 
-    for position_sampling_strategy in position_sampling_strategies:
-        for position_metric in position_metrics:
-            for k in top_k_gumbel_k:
-                for noise_coefficient in top_k_gumbel_noise_coefficient:
-                    for token_sampling_strategy in token_sampling_strategies:
-                        sampling_config = {
-                            "sampling": {
-                                "position_metric": position_metric,
-                                "position_sampling_strategy": position_sampling_strategy,
-                                "position_sampling_strategy_args": {
-                                    "top_k_gumbel": {
-                                        "k": k,
-                                        "gumbel_noise_coefficient": noise_coefficient
-                                    }
-                                },
-                                "token_sampling_strategy": token_sampling_strategy
+    for model, noise_schedule, ckpt_config, ckpt_path in zip(models, noise_schedules, ckpt_configs, ckpt_paths):
+        print(f"\n\n{'-'*10}Evaluating model from {ckpt_path} on {num_samples} samples{'-'*10}\n")
+        for position_sampling_strategy in position_sampling_strategies:
+            for position_metric in position_metrics:
+                for k in top_k_gumbel_k:
+                    for noise_coefficient in top_k_gumbel_noise_coefficient:
+                        for token_sampling_strategy in token_sampling_strategies:
+                            sampling_config = {
+                                "sampling": {
+                                    "position_metric": position_metric,
+                                    "position_sampling_strategy": position_sampling_strategy,
+                                    "position_sampling_strategy_args": {
+                                        "top_k_gumbel": {
+                                            "k": k,
+                                            "gumbel_noise_coefficient": noise_coefficient
+                                        }
+                                    },
+                                    "token_sampling_strategy": token_sampling_strategy
+                                }
                             }
-                        }
-                        sampling_config = dict_to_namespace(sampling_config)
+                            sampling_config = dict_to_namespace(sampling_config)
 
-                        sampler = get_sampler(ckpt_config, model, tokenizer, noise_schedule, sampling_config=sampling_config, compile_step=config.compilation.compile_torch, min_p=config.min_p)
-                        model.eval()
+                            sampler = get_sampler(ckpt_config, model, tokenizer, noise_schedule, sampling_config=sampling_config, compile_step=config.compilation.compile_torch, min_p=config.min_p)
+                            model.eval()
 
-                        all_samples = []
-                        for i in range(len(datasets)):
-                            samples = []
-                            with tqdm.tqdm(total=num_samples, desc="Sampling", dynamic_ncols=True) as pbar:
-                                with torch.no_grad(), torch.autocast(device.type, dtype=dtype):
-                                    for j in range(0, num_samples, config.batch_size):
-                                        bs = min(config.batch_size, num_samples - j)
-                                        # TODO: how is the max_length in SamplerInstance.model.config.max_seq_len set? Once that is done automatically for sudoku, no need to pass it here
-                                        if ckpt_config.training.use_diffusion_mask:
-                                            z_t = sampler.generate_from_given(all_puzzles_tokenized[i][j:j+bs], all_diffusion_mask[i][j:j+bs], config.num_denoising_steps, max_length=ckpt_config.model.max_seq_len, decode=False, show_progress=False, keep_history=False)
-                                        else:
-                                            z_t = sampler.generate_from_expected_t(all_puzzles_tokenized[i][j:j+bs], all_diffusion_mask[i][j:j+bs], config.num_denoising_steps, add_random_tokens=(ckpt_config.model.p_uniform == 0), max_length=ckpt_config.model.max_seq_len, decode=False, show_progress=False, keep_history=False)
-                                        samples.append(z_t)
-                                        pbar.update(bs)
-                            samples = torch.cat(samples, dim=0)
-                            samples = samples.cpu()
-                            all_samples.append(samples)
+                            all_samples = []
+                            for i in range(len(datasets)):
+                                samples = []
+                                with tqdm.tqdm(total=num_samples, desc="Sampling", dynamic_ncols=True) as pbar:
+                                    with torch.no_grad(), torch.autocast(device.type, dtype=dtype):
+                                        for j in range(0, num_samples, config.batch_size):
+                                            bs = min(config.batch_size, num_samples - j)
+                                            # TODO: how is the max_length in SamplerInstance.model.config.max_seq_len set? Once that is done automatically for sudoku, no need to pass it here
+                                            if ckpt_config.training.use_diffusion_mask:
+                                                z_t = sampler.generate_from_given(all_puzzles_tokenized[i][j:j+bs], all_diffusion_mask[i][j:j+bs], config.num_denoising_steps, max_length=ckpt_config.model.max_seq_len, decode=False, show_progress=False, keep_history=False)
+                                            else:
+                                                z_t = sampler.generate_from_expected_t(all_puzzles_tokenized[i][j:j+bs], all_diffusion_mask[i][j:j+bs], config.num_denoising_steps, add_random_tokens=(ckpt_config.model.p_uniform == 0), max_length=ckpt_config.model.max_seq_len, decode=False, show_progress=False, keep_history=False)
+                                            samples.append(z_t)
+                                            pbar.update(bs)
+                                samples = torch.cat(samples, dim=0)
+                                samples = samples.cpu()
+                                all_samples.append(samples)
 
-                        print(f"\nMetrics for config: {sampling_config_to_str(sampling_config)}:")
-                        for i in range(len(datasets)):
-                            metrics = score_sudoku(all_samples[i], all_diffusion_mask[i], all_solutions_tokenized[i], tokenizer)
-                            print(f"Dataset: {ds_names[i]}")
-                            for key, value in metrics.items():
-                                print(" " * 2 + f"{key}: {value:.4f}")
+                            print(f"\nMetrics for config: {sampling_config_to_str(sampling_config)}:")
+                            for i in range(len(datasets)):
+                                metrics = score_sudoku(all_samples[i], all_diffusion_mask[i], all_solutions_tokenized[i], tokenizer)
+                                print(f"Dataset: {ds_names[i]}")
+                                for key, value in metrics.items():
+                                    print(" " * 2 + f"{key}: {value:.4f}")
 
+                        if position_sampling_strategy != "top_k_gumbel":
+                            break
                     if position_sampling_strategy != "top_k_gumbel":
                         break
                 if position_sampling_strategy != "top_k_gumbel":
                     break
-            if position_metric != "top_k_gumbel":
-                break
 
 
 if __name__ == "__main__":
